4d615d864dd14fe7da29a41295bffeed7af0a791
Zura Guerra <guerrazuramx@gmail.com>
2016-02-22 23:36:03 -0600

    Register new admins

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,9 +1,25 @@
 defmodule FromSpace.AuthController do
   use FromSpace.Web, :controller
   alias FromSpace.Admin
+  alias FromSpace.AuthService
 
   def new(conn, _params) do
     changeset = Admin.changeset(%Admin{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def register(conn, %{"admin" => admin_params}) do
+    changeset = Admin.changeset(%Admin{}, admin_params)
+    case AuthService.create_admin(changeset) do
+      {:ok, admin} ->
+        conn
+        |> put_session(:admin, admin.id)
+        |> put_flash(:info, "Admin created")
+        |> redirect(to: "/admin/dashboard")
+      :else ->
+        conn
+        |> put_flash(:info, "Please try again")
+        |> redirect(to: "/new")
+    end
   end
 end
